9e88179f25c36542fb4636653a1c2f730e0c0e9a
coleslaws57 <mr.pc1129@gmail.com>
2026-06-02 20:30:30 -0500

    Add files via upload

---
+++ config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define ENCODER_A_PINS { GP3 }
+#define ENCODER_B_PINS { GP2 }
+#define ENCODER_RESOLUTION 4
---
+++ keymap.c
@@ -0,0 +1,19 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_A, KC_B, KC_C, KC_D
+    )
+};
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (clockwise) {
+        tap_code(KC_VOLU);
+    } else {
+        tap_code(KC_VOLD);
+    }
+    return false;
+}
